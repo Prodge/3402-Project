d97137124ee3d1cb76a94e9f3bf097307f0ad0cb
wimoMisterX <ran12us@gmail.com>
2016-10-29 00:29:02 +0800

    made block generation work with mpi

--- main.c
+++ main.c
@@ -1,101 +1,102 @@
 #include "header.h"
 
 int main(int argc, char* argv[]) {
+    int proc_id, ierr, num_procs;
+    MPI_Status status;
+
+    // Initialize MPI
+    ierr = MPI_Init(&argc, &argv);
+
+    // get # of processes
+    ierr = MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
+
+    // get induvidual proccess id
+    ierr = MPI_Comm_rank(MPI_COMM_WORLD, &proc_id);
+
+    int columns;
+    BlockArray* columns_block_array;
+
     debug("Checking arguments");
     check_arguments(argc, argv);
 
     debug("Reading files");
     char* data_filename = get_data_filename(argc, argv);
     int rows = get_num_rows_in_file(data_filename);
-    int columns = get_num_cols_in_file(data_filename, ',');
+    columns = get_num_cols_in_file(data_filename, ',');
     double** matrix = read_matrix(data_filename, rows, columns);
     double* keys = read_keys(get_keys_filename(argc, argv));
 
-    debug("Creating blocks for each column");
+    //debug("Creating blocks for each column");
+    int total = 0;
 
+    // if master
+    if (proc_id == 0){
+        // initialisze column block array
+        columns_block_array = malloc(columns * sizeof(BlockArray));
 
+        int avg_rows_per_proc = columns / num_procs;
 
-    int proc_id, ierr, num_procs;
-    MPI_Status status;
-
-    ierr = MPI_Init(&argc, &argv);
-    ierr = MPI_Comm_rank(MPI_COMM_WORLD, &proc_id);
-    ierr = MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
-
-    if( my_id == 0 ) {
-		// Block and wait for each machine to finish its caclulations and append them together
-        BlockArray* columns_block_array = malloc(columns * sizeof(BlockArray));
-        int this_column = 0;
+        // distributes columns to child processes
         for(int proc= 1; proc<num_procs; proc++){
-            int num_columns_to_receive;
-            ierr = MPI_Recv(&num_columns_to_receive, 1, MPI_INT, proc, 0, MPI_COMM_WORLD, &status);
+            int start_row = (proc-1) * avg_rows_per_proc;
+            int end_row = avg_rows_per_proc * proc;
+            ierr = MPI_Send( &start_row, 1 , MPI_INT, proc, 2000, MPI_COMM_WORLD);
+            ierr = MPI_Send( &end_row, 1 , MPI_INT, proc, 2000, MPI_COMM_WORLD);
+        }
 
-            // For each column, get all the blocks and add to the columns_block_array
-			for(int col = 1; col<num_columns_to_receive; col++){
-                int num_blocks_to_receive;
-                ierr = MPI_Recv(&num_blocks_to_receive, 1, MPI_INT, proc, 0, MPI_COMM_WORLD, &status);
+        // do work on master
+        int start_row = (num_procs-1)*avg_rows_per_proc;
+        int end_row = (num_procs*avg_rows_per_proc) + columns % num_procs;
+        printf("Proc %d recived %d --> %d\n", proc_id, start_row, end_row);
 
-                columns_block_array[this_column].length = num_blocks_to_recieve;
-                columns_block_array[this_column].array = malloc(num_blocks_to_recieve * sizeof(Block));
-                this_column ++;
+        for (int i=start_row; i<end_row; i++){
+            columns_block_array[i] = create_blocks_for_column(matrix[i], rows, keys, i);
+            total += columns_block_array[i].length;
+            printf("Column %d has %d blocks\n", i, columns_block_array[i].length);
+        }
 
-                ierr = MPI_Recv(&columns_block_array[this_column].array, sizeof(Block) * num_blocks_to_receive, MPI_BYTES, proc, 0, MPI_COMM_WORLD, &status);
+        // get results from workers
+        for(int proc= 1; proc<num_procs; proc++){
+            int start_row = (proc-1) * avg_rows_per_proc;
+            int end_row = avg_rows_per_proc * proc;
+            for (int j=start_row; j<end_row; j++){
+                ierr = MPI_Recv(&columns_block_array[j].length, 1, MPI_INT, proc, 2001, MPI_COMM_WORLD, &status);
+                columns_block_array[j].array = malloc(columns_block_array[j].length * sizeof(Block));
+                ierr = MPI_Recv(columns_block_array[j].array, sizeof(Block) * columns_block_array[j].length, MPI_BYTE, proc, 2001, MPI_COMM_WORLD, &status);
+                printf("Column %d has %d blocks\n", j, columns_block_array[j].length);
+                total += columns_block_array[j].length;
             }
         }
 
     }else{
-		// dispatch a portion of the columns to each machine
-        int start = (proc_id - 1) * (columns / (num_procs - 1));
-        int end = proc_id * (columns / (num_procs - 1));
-        if(end > columns){
-            end = columns
-        }
+        // get start and end rows for worker
+        int start_row, end_row;
+        ierr = MPI_Recv(&start_row, 1, MPI_INT, 0, 2000, MPI_COMM_WORLD, &status);
+        ierr = MPI_Recv(&end_row, 1, MPI_INT, 0, 2000, MPI_COMM_WORLD, &status);
+        printf("Proc %d recived %d --> %d\n", proc_id, start_row, end_row);
 
-        int total_columns = end - start - 1;
-        ierr = MPI_Send(&total_columns, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-
-        // Generate the blocks in each column and send the column back to the master node
-        for(int i = start; i < end; i++){
+        // do work and send work to master as it is done
+        for (int i=start_row; i<end_row; i++){
             BlockArray column_blocks = create_blocks_for_column(matrix[i], rows, keys, i);
-            ierr = MPI_Send(&column_blocks.length, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
-            ierr = MPI_Send(&column_blocks.array, sizeof(Block) * column_blocks.length, MPI_BYTES, 0, 0, MPI_COMM_WORLD);
+            ierr = MPI_Send(&column_blocks.length, 1, MPI_INT, 0, 2001, MPI_COMM_WORLD);
+            ierr = MPI_Send(column_blocks.array, sizeof(Block) * column_blocks.length, MPI_BYTE, 0, 2001, MPI_COMM_WORLD);
         }
-	}
+    }
 
-	MPI_Type_free(&mpi_car_type);
+    if (proc_id == 0){
+        debug("Finding collisions");
+        CollisionArray collisions = get_collisions(columns_block_array, columns);
+        print_collisions(collisions);
+
+        debug("Finding overlapping blocks");
+        int total_merged_blocks = merge_overlapping_blocks(collisions);
+
+        printf("--------------------S U M M A R Y--------------------\n");
+        printf("Total number of blocks generated = %d\nTotal number of collisions found = %d\nTotal number of merged blocks = %d\n", total, collisions.length, total_merged_blocks);
+        printf("-----------------------------------------------------\n");
+    }
+
     ierr = MPI_Finalize();
-
-
-
-
-    /*BlockArray* columns_block_array = malloc(columns * sizeof(BlockArray));*/
-    /*int i;*/
-    /*int total = 0;*/
-    /*omp_set_nested(1);*/
-    /*#pragma omp parallel num_threads(sysconf(_SC_NPROCESSORS_ONLN))*/
-    /*{*/
-        /*#pragma omp for private(i)*/
-        /*for (i=0; i<columns; i++){*/
-            /*columns_block_array[i] = create_blocks_for_column(matrix[i], rows, keys, i);*/
-            /*printf("Column %d has %d blocks\n", i, columns_block_array[i].length);*/
-            /*#pragma omp atomic*/
-            /*total += columns_block_array[i].length;*/
-        /*}*/
-    /*}*/
-
-
-
-
-    debug("Finding collisions");
-    CollisionArray collisions = get_collisions(columns_block_array, columns);
-    print_collisions(collisions);
-
-    debug("Finding overlapping blocks");
-    int total_merged_blocks = merge_overlapping_blocks(collisions);
-
-    printf("--------------------S U M M A R Y--------------------\n");
-    printf("Total number of blocks generated = %d\nTotal number of collisions found = %d\nTotal number of merged blocks = %d\n", total, collisions.length, total_merged_blocks);
-    printf("-----------------------------------------------------\n");
 
     return 0;
 }
