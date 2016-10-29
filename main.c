9158fdcb8b1e8c4e33c5d9e60f76c904b561c6ef
wimoMisterX <ran12us@gmail.com>
2016-10-29 08:47:17 +0800

    created mpi struct for block, cleaned up a bit

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include "header.h"
 
 int main(int argc, char* argv[]) {
-    int proc_id, ierr, num_procs;
+    int proc_id, ierr, num_procs, start_row, end_row;
+    int total = 0;
     MPI_Status status;
+    BlockArray* columns_block_array;
 
     // Initialize MPI
     ierr = MPI_Init(&argc, &argv);
@@ -13,8 +15,13 @@ int main(int argc, char* argv[]) {
     // get induvidual proccess id
     ierr = MPI_Comm_rank(MPI_COMM_WORLD, &proc_id);
 
-    int columns;
-    BlockArray* columns_block_array;
+    // create mpi struct for block
+    int blocklengths[3] = {1, 4, 1};
+    MPI_Datatype types[3] = {MPI_DOUBLE, MPI_INT, MPI_INT};
+    MPI_Datatype mpi_block_type;
+    MPI_Aint offsets[3] = {offsetof(Block, signature), offsetof(Block, row_ids), offsetof(Block, column_number)};
+    MPI_Type_create_struct(3, blocklengths, offsets, types, &mpi_block_type);
+    MPI_Type_commit(&mpi_block_type);
 
     debug("Checking arguments");
     check_arguments(argc, argv);
@@ -22,12 +29,11 @@ int main(int argc, char* argv[]) {
     debug("Reading files");
     char* data_filename = get_data_filename(argc, argv);
     int rows = get_num_rows_in_file(data_filename);
-    columns = get_num_cols_in_file(data_filename, ',');
+    int columns = get_num_cols_in_file(data_filename, ',');
     double** matrix = read_matrix(data_filename, rows, columns);
     double* keys = read_keys(get_keys_filename(argc, argv));
 
-    //debug("Creating blocks for each column");
-    int total = 0;
+    debug("Creating blocks for each column");
 
     // if master
     if (proc_id == 0){
@@ -38,15 +44,15 @@ int main(int argc, char* argv[]) {
 
         // distributes columns to child processes
         for(int proc= 1; proc<num_procs; proc++){
-            int start_row = (proc-1) * avg_rows_per_proc;
-            int end_row = avg_rows_per_proc * proc;
+            start_row = (proc-1) * avg_rows_per_proc;
+            end_row = avg_rows_per_proc * proc;
             ierr = MPI_Send( &start_row, 1 , MPI_INT, proc, 2000, MPI_COMM_WORLD);
             ierr = MPI_Send( &end_row, 1 , MPI_INT, proc, 2000, MPI_COMM_WORLD);
         }
 
         // do work on master
-        int start_row = (num_procs-1)*avg_rows_per_proc;
-        int end_row = (num_procs*avg_rows_per_proc) + columns % num_procs;
+        start_row = (num_procs-1)*avg_rows_per_proc;
+        end_row = (num_procs*avg_rows_per_proc) + columns % num_procs;
         printf("Proc %d recived %d --> %d\n", proc_id, start_row, end_row);
 
         for (int i=start_row; i<end_row; i++){
@@ -57,12 +63,12 @@ int main(int argc, char* argv[]) {
 
         // get results from workers
         for(int proc= 1; proc<num_procs; proc++){
-            int start_row = (proc-1) * avg_rows_per_proc;
-            int end_row = avg_rows_per_proc * proc;
+            start_row = (proc-1) * avg_rows_per_proc;
+            end_row = avg_rows_per_proc * proc;
             for (int j=start_row; j<end_row; j++){
                 ierr = MPI_Recv(&columns_block_array[j].length, 1, MPI_INT, proc, 2001, MPI_COMM_WORLD, &status);
                 columns_block_array[j].array = malloc(columns_block_array[j].length * sizeof(Block));
-                ierr = MPI_Recv(columns_block_array[j].array, sizeof(Block) * columns_block_array[j].length, MPI_BYTE, proc, 2001, MPI_COMM_WORLD, &status);
+                ierr = MPI_Recv(columns_block_array[j].array, columns_block_array[j].length, mpi_block_type, proc, 2001, MPI_COMM_WORLD, &status);
                 printf("Column %d has %d blocks\n", j, columns_block_array[j].length);
                 total += columns_block_array[j].length;
             }
@@ -70,7 +76,6 @@ int main(int argc, char* argv[]) {
 
     }else{
         // get start and end rows for worker
-        int start_row, end_row;
         ierr = MPI_Recv(&start_row, 1, MPI_INT, 0, 2000, MPI_COMM_WORLD, &status);
         ierr = MPI_Recv(&end_row, 1, MPI_INT, 0, 2000, MPI_COMM_WORLD, &status);
         printf("Proc %d recived %d --> %d\n", proc_id, start_row, end_row);
@@ -79,7 +84,7 @@ int main(int argc, char* argv[]) {
         for (int i=start_row; i<end_row; i++){
             BlockArray column_blocks = create_blocks_for_column(matrix[i], rows, keys, i);
             ierr = MPI_Send(&column_blocks.length, 1, MPI_INT, 0, 2001, MPI_COMM_WORLD);
-            ierr = MPI_Send(column_blocks.array, sizeof(Block) * column_blocks.length, MPI_BYTE, 0, 2001, MPI_COMM_WORLD);
+            ierr = MPI_Send(column_blocks.array, column_blocks.length, mpi_block_type, 0, 2001, MPI_COMM_WORLD);
         }
     }
 
