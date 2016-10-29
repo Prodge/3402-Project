e6cab90739f2828f80a81c44b0fa117d323f4d58
wimoMisterX <ran12us@gmail.com>
2016-10-29 23:16:37 +0800

    cleaned up code

--- main.c
+++ main.c
@@ -6,6 +6,17 @@ int main(int argc, char* argv[]) {
     MPI_Status status;
     BlockArray* columns_block_array;
 
+    // create mpi struct for block
+    int blocklengths[3] = {1, 4, 1};
+    MPI_Datatype types[3] = {MPI_DOUBLE, MPI_INT, MPI_INT};
+    MPI_Datatype mpi_block_type;
+    MPI_Aint offsets[3] = {offsetof(Block, signature), offsetof(Block, row_ids), offsetof(Block, column_number)};
+    MPI_Type_create_struct(3, blocklengths, offsets, types, &mpi_block_type);
+    MPI_Type_commit(&mpi_block_type);
+
+    // enable nested parallelization
+    omp_set_nested(1);
+
     // Initialize MPI
     ierr = MPI_Init(&argc, &argv);
 
@@ -15,39 +26,25 @@ int main(int argc, char* argv[]) {
     // get induvidual proccess id
     ierr = MPI_Comm_rank(MPI_COMM_WORLD, &proc_id);
 
-    // create mpi struct for block
-    int blocklengths[3] = {1, 4, 1};
-    MPI_Datatype types[3] = {MPI_DOUBLE, MPI_INT, MPI_INT};
-    MPI_Datatype mpi_block_type;
-    MPI_Aint offsets[3] = {offsetof(Block, signature), offsetof(Block, row_ids), offsetof(Block, column_number)};
-    MPI_Type_create_struct(3, blocklengths, offsets, types, &mpi_block_type);
-    MPI_Type_commit(&mpi_block_type);
-
-    debug("Checking arguments");
+    printf("Checking arguments in process %d\n", proc_id);
     check_arguments(argc, argv);
 
-    debug("Reading files");
+    printf("Reading files in process %d\n", proc_id);
     char* data_filename = get_data_filename(argc, argv);
     int rows = get_num_rows_in_file(data_filename);
     int columns = get_num_cols_in_file(data_filename, ',');
     double** matrix = read_matrix(data_filename, rows, columns);
     double* keys = read_keys(get_keys_filename(argc, argv));
 
-    debug("Creating blocks for each column");
-
-    omp_set_nested(1);
-	
-	int avg_rows_per_proc = columns / (num_procs-1);
-
     // initialisze column block array
     columns_block_array = malloc(columns * sizeof(BlockArray));
+    int avg_rows_per_proc = columns / (num_procs-1);
 
-    // if master
     if (proc_id == 0){
         // get results from workers
         for(int proc= 1; proc<num_procs; proc++){
             start_row = (proc-1) * avg_rows_per_proc;
-        	end_row = (proc == (num_procs-1)) ? (avg_rows_per_proc*proc) + (columns % (num_procs-1)) : avg_rows_per_proc * proc;
+            end_row = (proc == (num_procs-1)) ? (avg_rows_per_proc*proc) + (columns % (num_procs-1)) : avg_rows_per_proc * proc;
             for (int j=start_row; j<end_row; j++){
                 ierr = MPI_Recv(&columns_block_array[j].length, 1, MPI_INT, proc, 2001, MPI_COMM_WORLD, &status);
                 columns_block_array[j].array = malloc(columns_block_array[j].length * sizeof(Block));
@@ -57,75 +54,79 @@ int main(int argc, char* argv[]) {
             }
         }
     }else{
+        printf("Creating blocks for column in process %d\n", proc_id);
         // get start and end rows for worker
         start_row = (proc_id-1) * avg_rows_per_proc;
         end_row = (proc_id == (num_procs-1)) ? (avg_rows_per_proc*proc_id) + (columns % (num_procs-1)) : avg_rows_per_proc * proc_id;
 
-        // do work and send work to master as it is done
+        // create blocks
         BlockArray* worker_columns_block_array = malloc((end_row-start_row) * sizeof(BlockArray));
-		int counter = 0;
+        int counter = 0;
         #pragma omp parallel num_threads(sysconf(_SC_NPROCESSORS_ONLN))
         {
             #pragma omp for ordered schedule(dynamic) private(i)
-			for (i=start_row; i<end_row; i++){
-				#pragma omp ordered
-				worker_columns_block_array[counter] = create_blocks_for_column(matrix[i], rows, keys, i);
-				#pragma omp critical
-				counter++;
-			}
+            for (i=start_row; i<end_row; i++){
+                #pragma omp ordered
+                worker_columns_block_array[counter] = create_blocks_for_column(matrix[i], rows, keys, i);
+                #pragma omp critical
+                counter++;
+            }
         }
 
-		for (counter=0; counter<(end_row-start_row); counter++){
+        // send blocks to master
+        for (counter=0; counter<(end_row-start_row); counter++){
             ierr = MPI_Send(&worker_columns_block_array[counter].length, 1, MPI_INT, 0, 2001, MPI_COMM_WORLD);
             ierr = MPI_Send(worker_columns_block_array[counter].array, worker_columns_block_array[counter].length, mpi_block_type, 0, 2001, MPI_COMM_WORLD);
-			free(worker_columns_block_array[counter].array);
-		}
-		free(worker_columns_block_array);
+            free(worker_columns_block_array[counter].array);
+        }
+        free(worker_columns_block_array);
     }
 
-	MPI_Barrier(MPI_COMM_WORLD);
-	for (i=0; i<columns; i++){
-		MPI_Bcast(&columns_block_array[i].length, 1, MPI_INT, 0, MPI_COMM_WORLD);
-		if (proc_id != 0){
-       		columns_block_array[i].array = malloc(columns_block_array[i].length * sizeof(Block));
-		}
-		MPI_Bcast(columns_block_array[i].array, columns_block_array[i].length, mpi_block_type, 0, MPI_COMM_WORLD);
-	}
+    // wait for all processes to reach then broadcast columns_block_array to workers
+    MPI_Barrier(MPI_COMM_WORLD);
+    for (i=0; i<columns; i++){
+        MPI_Bcast(&columns_block_array[i].length, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        if (proc_id != 0){
+            columns_block_array[i].array = malloc(columns_block_array[i].length * sizeof(Block));
+        }
+        MPI_Bcast(columns_block_array[i].array, columns_block_array[i].length, mpi_block_type, 0, MPI_COMM_WORLD);
+    }
 
-	CollisionArray collisions = get_collisions(columns_block_array, columns, proc_id, num_procs);
+    printf("Creating collisions in process %d\n", proc_id);
+    CollisionArray collisions = get_collisions(columns_block_array, columns, proc_id, num_procs);
 
     if (proc_id == 0){
-        debug("Finding collisions");
         print_collisions(collisions);
     }
-	
-	MPI_Barrier(MPI_COMM_WORLD);
-    MPI_Bcast(&collisions.length, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	if (proc_id != 0){
-		free(collisions.array);
-		collisions.array= malloc(collisions.length * sizeof(Collision));
-	}
-	MPI_Barrier(MPI_COMM_WORLD);
-	for (i=0; i<collisions.length; i++){
-		MPI_Bcast(&collisions.array[i].length, 1, MPI_INT, 0, MPI_COMM_WORLD);
-		if (proc_id != 0){
-			collisions.array[i].columns = malloc(collisions.array[i].length * sizeof(int));
-		}
-		MPI_Bcast(collisions.array[i].columns, collisions.array[i].length, MPI_INT, 0, MPI_COMM_WORLD);
-		MPI_Bcast(&collisions.array[i].signature, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-		MPI_Bcast(&collisions.array[i].row_ids, 4, MPI_INT, 0, MPI_COMM_WORLD);
-	}
 
+    // wait for all processes to reach then broadcast collisions to workers
+    MPI_Barrier(MPI_COMM_WORLD);
+    MPI_Bcast(&collisions.length, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    if (proc_id != 0){
+        free(collisions.array);
+        collisions.array= malloc(collisions.length * sizeof(Collision));
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+    for (i=0; i<collisions.length; i++){
+        MPI_Bcast(&collisions.array[i].length, 1, MPI_INT, 0, MPI_COMM_WORLD);
+        if (proc_id != 0){
+            collisions.array[i].columns = malloc(collisions.array[i].length * sizeof(int));
+        }
+        MPI_Bcast(collisions.array[i].columns, collisions.array[i].length, MPI_INT, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&collisions.array[i].signature, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(&collisions.array[i].row_ids, 4, MPI_INT, 0, MPI_COMM_WORLD);
+    }
+
+    printf("Finding overlapping blocks in process %d\n", proc_id);
     int total_merged_blocks = merge_overlapping_blocks(collisions, proc_id, num_procs);
 
     if (proc_id == 0){
-        debug("Finding overlapping blocks");
-
         printf("--------------------S U M M A R Y--------------------\n");
         printf("Total number of blocks generated = %d\nTotal number of collisions found = %d\nTotal number of merged blocks = %d\n", total, collisions.length, total_merged_blocks);
         printf("-----------------------------------------------------\n");
     }
 
+    // end mpi
     ierr = MPI_Finalize();
 
     return 0;
--- matches.c
+++ matches.c
@@ -64,97 +64,97 @@ int remove_duplicates_and_print_overlapping_blocks(Match * overlapping_blocks_co
 
 /*returns number of overlapping blocks given collisions*/
 int merge_overlapping_blocks(CollisionArray collisions, int proc_id, int num_procs){
-	MPI_Status status;
+    MPI_Status status;
     Match * overlapping_blocks_column;
-	int total = 0;
-	int avg_rows_per_proc = collisions.length / (num_procs-1);
+    int total = 0;
+    int avg_rows_per_proc = collisions.length / (num_procs-1);
 
-	if (proc_id == 0){
-    	// Creates an array of matches for each collision
-   		overlapping_blocks_column = malloc(collisions.length * sizeof(Match));
-		for (int proc=1; proc<num_procs; proc++){
-			int length;
-        	MPI_Recv(&length, 1, MPI_INT, proc, 2002, MPI_COMM_WORLD, &status);
-			for (int k=0; k<length; k++){
-        		MPI_Recv(&overlapping_blocks_column[total].columns_length, 1, MPI_INT, proc, 2002, MPI_COMM_WORLD, &status);
-				overlapping_blocks_column[total].columns = malloc(sizeof(int) * overlapping_blocks_column[total].columns_length);
-        		MPI_Recv(overlapping_blocks_column[total].columns, overlapping_blocks_column[total].columns_length, MPI_INT, proc, 2002, MPI_COMM_WORLD, &status);
-        		MPI_Recv(&overlapping_blocks_column[total].row_ids_length, 1, MPI_INT, proc, 2002, MPI_COMM_WORLD, &status);
-				overlapping_blocks_column[total].row_ids = malloc(sizeof(int) * overlapping_blocks_column[total].row_ids_length);
-        		MPI_Recv(overlapping_blocks_column[total].row_ids, overlapping_blocks_column[total].row_ids_length, MPI_INT, proc, 2002, MPI_COMM_WORLD, &status);
-				total++;
-			}
-		}
-		overlapping_blocks_column = realloc(overlapping_blocks_column, total * sizeof(Match));
-	}else{
+    if (proc_id == 0){
+        // Creates an array of matches for each collision
+        overlapping_blocks_column = malloc(collisions.length * sizeof(Match));
+        for (int proc=1; proc<num_procs; proc++){
+            int length;
+            MPI_Recv(&length, 1, MPI_INT, proc, 2002, MPI_COMM_WORLD, &status);
+            for (int k=0; k<length; k++){
+                MPI_Recv(&overlapping_blocks_column[total].columns_length, 1, MPI_INT, proc, 2002, MPI_COMM_WORLD, &status);
+                overlapping_blocks_column[total].columns = malloc(sizeof(int) * overlapping_blocks_column[total].columns_length);
+                MPI_Recv(overlapping_blocks_column[total].columns, overlapping_blocks_column[total].columns_length, MPI_INT, proc, 2002, MPI_COMM_WORLD, &status);
+                MPI_Recv(&overlapping_blocks_column[total].row_ids_length, 1, MPI_INT, proc, 2002, MPI_COMM_WORLD, &status);
+                overlapping_blocks_column[total].row_ids = malloc(sizeof(int) * overlapping_blocks_column[total].row_ids_length);
+                MPI_Recv(overlapping_blocks_column[total].row_ids, overlapping_blocks_column[total].row_ids_length, MPI_INT, proc, 2002, MPI_COMM_WORLD, &status);
+                total++;
+            }
+        }
+        overlapping_blocks_column = realloc(overlapping_blocks_column, total * sizeof(Match));
+    }else{
         // get start and end rows for worker
         int start_row = (proc_id-1) * avg_rows_per_proc;
         int end_row = (proc_id == (num_procs-1)) ? (avg_rows_per_proc*proc_id) + (collisions.length % (num_procs-1)) : avg_rows_per_proc * proc_id;
 
-   		overlapping_blocks_column = malloc((end_row-start_row) * sizeof(Match));
+        overlapping_blocks_column = malloc((end_row-start_row) * sizeof(Match));
 
-		int start_match; // Loop counter needs to be declared outside of openmp loop
-		int counter = 0;
-		#pragma omp parallel num_threads(sysconf(_SC_NPROCESSORS_ONLN))
-		{
-			#pragma omp for private(start_match)
-			for (start_match=start_row; start_match<end_row; start_match++){
-				// Creates inital match
-				Match overlapping_match = get_initial_match(collisions.array[start_match].row_ids, collisions.array[start_match].columns, collisions.array[start_match].length);
-					// From the next collision in the collision array
-					for (int other_match=(start_match+1); other_match<collisions.length; other_match++){
-					// If the columns in each collision match and there are overlaps in row_ids then
-					// store row_ids in the match
-					if (collisions.array[start_match].length == collisions.array[other_match].length &&
-						get_number_of_repeated_elements(
-							collisions.array[start_match].columns, collisions.array[start_match].length,
-							collisions.array[other_match].columns, collisions.array[other_match].length
-						) == collisions.array[start_match].length &&
-						get_number_of_repeated_elements(collisions.array[start_match].row_ids, 4, collisions.array[other_match].row_ids, 4) >= 2
-					){
-						overlapping_match.row_ids = realloc(overlapping_match.row_ids, (overlapping_match.row_ids_length + 4) * sizeof(int));
-						for (int a=0; a<4; a++){
-							overlapping_match.row_ids[overlapping_match.row_ids_length] = collisions.array[other_match].row_ids[a];
-							overlapping_match.row_ids_length++;
-						}
-					}
-				}
-				// If a match was found then
-				if (overlapping_match.row_ids_length > 4){
-					// Makes row_ids and columns unique and sorted
-					Int1DArray row_ids = get_unique_array(overlapping_match.row_ids, overlapping_match.row_ids_length);
-					free(overlapping_match.row_ids);
-					overlapping_match.row_ids = row_ids.array;
-					overlapping_match.row_ids_length = row_ids.length;
-					Int1DArray columns = get_unique_array(overlapping_match.columns, overlapping_match.columns_length);
-					free(overlapping_match.columns);
-					overlapping_match.columns = columns.array;
-					overlapping_match.columns_length = columns.length;
-					#pragma omp critical
-					counter++;
-				}else{
-					// Remove unsed memory
-					free(overlapping_match.row_ids);
-					free(overlapping_match.columns);
-					// Add -1 to length to indicate no match for this collision
-					overlapping_match.row_ids_length = -1;
-					overlapping_match.columns_length = -1;
-				}
-				// Add match to match array
-				overlapping_blocks_column[end_row-start_match-1] = overlapping_match;
-			}
-		}
+        int start_match; // Loop counter needs to be declared outside of openmp loop
+        int counter = 0;
+        #pragma omp parallel num_threads(sysconf(_SC_NPROCESSORS_ONLN))
+        {
+            #pragma omp for private(start_match)
+            for (start_match=start_row; start_match<end_row; start_match++){
+                // Creates inital match
+                Match overlapping_match = get_initial_match(collisions.array[start_match].row_ids, collisions.array[start_match].columns, collisions.array[start_match].length);
+                    // From the next collision in the collision array
+                    for (int other_match=(start_match+1); other_match<collisions.length; other_match++){
+                    // If the columns in each collision match and there are overlaps in row_ids then
+                    // store row_ids in the match
+                    if (collisions.array[start_match].length == collisions.array[other_match].length &&
+                        get_number_of_repeated_elements(
+                            collisions.array[start_match].columns, collisions.array[start_match].length,
+                            collisions.array[other_match].columns, collisions.array[other_match].length
+                        ) == collisions.array[start_match].length &&
+                        get_number_of_repeated_elements(collisions.array[start_match].row_ids, 4, collisions.array[other_match].row_ids, 4) >= 2
+                    ){
+                        overlapping_match.row_ids = realloc(overlapping_match.row_ids, (overlapping_match.row_ids_length + 4) * sizeof(int));
+                        for (int a=0; a<4; a++){
+                            overlapping_match.row_ids[overlapping_match.row_ids_length] = collisions.array[other_match].row_ids[a];
+                            overlapping_match.row_ids_length++;
+                        }
+                    }
+                }
+                // If a match was found then
+                if (overlapping_match.row_ids_length > 4){
+                    // Makes row_ids and columns unique and sorted
+                    Int1DArray row_ids = get_unique_array(overlapping_match.row_ids, overlapping_match.row_ids_length);
+                    free(overlapping_match.row_ids);
+                    overlapping_match.row_ids = row_ids.array;
+                    overlapping_match.row_ids_length = row_ids.length;
+                    Int1DArray columns = get_unique_array(overlapping_match.columns, overlapping_match.columns_length);
+                    free(overlapping_match.columns);
+                    overlapping_match.columns = columns.array;
+                    overlapping_match.columns_length = columns.length;
+                    #pragma omp critical
+                    counter++;
+                }else{
+                    // Remove unsed memory
+                    free(overlapping_match.row_ids);
+                    free(overlapping_match.columns);
+                    // Add -1 to length to indicate no match for this collision
+                    overlapping_match.row_ids_length = -1;
+                    overlapping_match.columns_length = -1;
+                }
+                // Add match to match array
+                overlapping_blocks_column[end_row-start_match-1] = overlapping_match;
+            }
+        }
         MPI_Send(&counter, 1, MPI_INT, 0, 2002, MPI_COMM_WORLD);
-		for (int i=(end_row-start_row-1); i>-1; i--){
-			if (overlapping_blocks_column[i].columns_length != -1 && overlapping_blocks_column[i].row_ids_length != -1){
-        		MPI_Send(&overlapping_blocks_column[i].columns_length, 1, MPI_INT, 0, 2002, MPI_COMM_WORLD);
-        		MPI_Send(overlapping_blocks_column[i].columns, overlapping_blocks_column[i].columns_length, MPI_INT, 0, 2002, MPI_COMM_WORLD);
-        		MPI_Send(&overlapping_blocks_column[i].row_ids_length, 1, MPI_INT, 0, 2002, MPI_COMM_WORLD);
-        		MPI_Send(overlapping_blocks_column[i].row_ids, overlapping_blocks_column[i].row_ids_length, MPI_INT, 0, 2002, MPI_COMM_WORLD);
-			}
-		}
-		return -1;
-	}
+        for (int i=(end_row-start_row-1); i>-1; i--){
+            if (overlapping_blocks_column[i].columns_length != -1 && overlapping_blocks_column[i].row_ids_length != -1){
+                MPI_Send(&overlapping_blocks_column[i].columns_length, 1, MPI_INT, 0, 2002, MPI_COMM_WORLD);
+                MPI_Send(overlapping_blocks_column[i].columns, overlapping_blocks_column[i].columns_length, MPI_INT, 0, 2002, MPI_COMM_WORLD);
+                MPI_Send(&overlapping_blocks_column[i].row_ids_length, 1, MPI_INT, 0, 2002, MPI_COMM_WORLD);
+                MPI_Send(overlapping_blocks_column[i].row_ids, overlapping_blocks_column[i].row_ids_length, MPI_INT, 0, 2002, MPI_COMM_WORLD);
+            }
+        }
+        return -1;
+    }
 
     // return total number of unique overlapping blocks
     return remove_duplicates_and_print_overlapping_blocks(overlapping_blocks_column, total);
